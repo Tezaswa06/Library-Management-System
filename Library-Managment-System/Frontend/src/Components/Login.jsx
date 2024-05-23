dff409d5c48d861718f00e12ecaf2a038757bb6f
LavishSainik <lavishsainik@gmail.com>
2024-05-23 14:57:25 +0530

    login and resgiter page

--- Library-Managment-System/Frontend/src/Components/Login.jsx
+++ Library-Managment-System/Frontend/src/Components/Login.jsx
@@ -1,51 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Form, Input, Button, Card } from 'antd';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-
-
-const Login = ({setIsAuthenticated}) => {
-    const [form] = Form.useForm();
+const Login = ({ setIsAuthenticated }) => {
+  const [form] = Form.useForm();
   const navigate = useNavigate();
 
-  const handleLogin = async(values) => {
-    // Add your authentication logic here
+  const handleLogin = async (values) => {
     const { username, password } = values;
     let payload = {
-        email : username,
-        password : password
+      email: username,
+      password: password,
+    };
+    let apiResponse = await axios.post("http://localhost:8081/api/auth/loginStudent", payload);
+    if (apiResponse) {
+      console.log(apiResponse);
+      localStorage.setItem('user', JSON.stringify({ username }));
+      setIsAuthenticated(true);
+      navigate('/Profile');
     }
-    let apiResponse = await axios.post("http://localhost:8081/api/auth/loginStudent",payload)
-    if(apiResponse){
-        console.log(apiResponse);
-        localStorage.setItem('user', JSON.stringify({ username }));
-        setIsAuthenticated(true)
-        navigate('/Profile')
-    }
-    
-
-
   };
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <Card className="w-full max-w-md">
-        <h2 className="text-2xl font-bold text-center mb-4">Login</h2>
-        <Form form={form} onFinish={handleLogin}>
-          <Form.Item name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
-            <Input placeholder="Studentemail" />
-          </Form.Item>
-          <Form.Item name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
-            <Input.Password placeholder="Password" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" className="w-full">Login</Button>
-          </Form.Item>
-        </Form>
-      </Card>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 bg-no-repeat bg-cover bg-center relative" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1507842217343-583bb7270b66?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8c2VhcmNofDF8fGxpYnJhcnl8ZW58MHx8fHwxNjU4NDAwNTQ4&ixlib=rb-1.2.1&q=80&w=2000')" }}>
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative z-10 w-full max-w-md p-8">
+        <div className="mb-8 text-center">
+   
+          <h1 className="text-4xl font-bold text-white">LIBRARY MANAGEMENT</h1>
+        </div>
+        <Card className="bg-white bg-opacity-90 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold text-center mb-6 text-green-600">Login</h2>
+          <Form form={form} onFinish={handleLogin}>
+            <Form.Item name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
+              <Input placeholder="Student Email" className="rounded-lg" />
+            </Form.Item>
+            <Form.Item name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+              <Input.Password placeholder="Password" className="rounded-lg" />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="font-bold w-full bg-green-600 hover:bg-green-500 border-none rounded-lg">Login</Button>
+            <Link to='/register'> <Button type="primary" htmlType="submit" className="w-full font-bold bg-green-600 hover:bg-green-500 border-none rounded-lg my-2">Register Now</Button></Link> 
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default Login
+export default Login;
--- Library-Managment-System/Frontend/src/Components/Register.jsx
+++ Library-Managment-System/Frontend/src/Components/Register.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Form, Input, Button, Card } from 'antd'; // Import necessary components from Ant Design
+import { Form, Input, Button, Card, notification } from 'antd';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
-  const [form] = Form.useForm(); // Initialize form
+  const [form] = Form.useForm();
+  const [api, contextHolder] = notification.useNotification();
 
   const handleRegister = async (values) => {
-    // Add your authentication logic here
     const { username, password, StudentName, RollNumber } = values;
     let payload = {
       email: username,
@@ -17,49 +18,71 @@ const Register = () => {
     try {
       let apiResponse = await axios.post("http://localhost:8081/api/auth/registerStudent", payload);
       if (apiResponse) {
-        console.log(apiResponse.data); // Log the response data
+        console.log(apiResponse.data);
+        api.open({
+          message: 'Register Successful',
+          description: 'You have been registered successfully. Now you can login using your credentials.',
+          duration: 10,
+        });
       }
     } catch (error) {
       console.error("Error during registration:", error);
+      api.open({
+        message: 'Register Failed',
+        description: 'An error occurred during registration. Please try again later.',
+        duration: 10,
+      });
     }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <Card className="w-full max-w-md">
-        <h2 className="text-2xl font-bold text-center mb-4">Register</h2>
-        <Form form={form} onFinish={handleRegister}>
-          <Form.Item
-            name="username"
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <Input placeholder="Student Email" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password placeholder="Password" />
-          </Form.Item>
-          <Form.Item
-            name="StudentName"
-            rules={[{ required: true, message: 'Please input your name!' }]}
-          >
-            <Input placeholder="Student Name" />
-          </Form.Item>
-          <Form.Item
-            name="RollNumber"
-            rules={[{ required: true, message: 'Please input your roll number!' }]}
-          >
-            <Input placeholder="Roll Number" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" className="w-full">
-              Register
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 bg-cover bg-center relative" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1507842217343-583bb7270b66?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8c2VhcmNofDF8fGxpYnJhcnl8ZW58MHx8fHwxNjU4NDAwNTQ4&ixlib=rb-1.2.1&q=80&w=2000')" }}>
+      {contextHolder}
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative z-10 w-full max-w-md p-8">
+        <div className="mb-8 text-center">
+          <h1 className="text-4xl font-extrabold text-white">LIBRARY MANAGEMENT</h1>
+        </div>
+        <Card className="bg-white bg-opacity-90 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold text-center mb-6 text-green-600">Register</h2>
+          <Form form={form} onFinish={handleRegister}>
+            <Form.Item
+              name="username"
+              rules={[{ required: true, message: 'Please input your email!' }]}
+            >
+              <Input placeholder="Student Email" className="rounded-lg" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password placeholder="Password" className="rounded-lg" />
+            </Form.Item>
+            <Form.Item
+              name="StudentName"
+              rules={[{ required: true, message: 'Please input your name!' }]}
+            >
+              <Input placeholder="Student Name" className="rounded-lg" />
+            </Form.Item>
+            <Form.Item
+              name="RollNumber"
+              rules={[{ required: true, message: 'Please input your roll number!' }]}
+            >
+              <Input placeholder="Roll Number" className="rounded-lg" />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="w-full bg-green-600 hover:bg-green-500 border-none rounded-lg">
+                Register
+              </Button>
+              <Link to='/'>
+                <Button type="default" className="w-full my-2 text-white font-bold bg-green-600 hover:bg-green-500 border-none rounded-lg">
+                  Login Now
+                </Button>
+              </Link>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
     </div>
   );
 };
